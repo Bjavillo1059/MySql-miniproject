de7c88afd4aec40cd5ff6d2d4cd11cbd4daa6ecf
Brandon Javillo <bjavillo1059@gmail.com>
2021-09-14 19:47:58 -0700

    changes made

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'ChristiaN7766@#',
+        password: '',
         database: 'movies_db'
     },
     console.log(`Connected to the movies_db database.`)
